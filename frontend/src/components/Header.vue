b5da289f23a90afd54f2781d95459ba5b204ba2c
Samlxrd <samlord.dzn@gmail.com>
2024-06-15 10:44:06 -0300

    first commit

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+  <header>
+    <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/other">Outro</RouterLink>
+        <a href="">Disponibilidade</a>
+        <a href="">Pedidos</a>
+        <a href="">Atendimento</a>
+        <a href="">Entregas</a>
+        <a href="">Agendamento</a>
+        <a href="">Login</a>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/OtherView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>Olá mundo2</h1>
+  </main>
+</template>
