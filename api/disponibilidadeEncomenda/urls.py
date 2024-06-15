6298d1305b410cd0a4e60c2f927c9c7deffbbcd6
Samlxrd <samlord.dzn@gmail.com>
2024-06-15 13:21:34 -0300

    created disponibility module

---
+++ api/disponibilidadeEncomenda/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from .views import DisponibilidadeViewSet, DisponExcecaoViewSet
+
+urlpatterns = [
+    path('getDisponibilidade', DisponibilidadeViewSet.list_disponibilidade, name='getDisponibilidade'),
+    path('getDisponibilidade/<int:id>/', DisponibilidadeViewSet.get_disponibilidade, name='getDisponibilidadeId'),
+    path('postDisponibilidade', DisponibilidadeViewSet.post_disponibilidade, name='postDisponibilidade'),
+    path('patchDisponibilidade/<int:id>/', DisponibilidadeViewSet.patch_disponibilidade, name='patchDisponibilidade'),
+    path('deleteDisponibilidade/<int:id>/', DisponibilidadeViewSet.delete_disponibilidade, name='deleteDisponibilidade'),
+
+    path('getDisponExcecao', DisponExcecaoViewSet.list_disponibilidade_excecao, name='getDisponExcecao'),
+    path('getDisponExcecao/<int:id>/', DisponExcecaoViewSet.get_disponibilidade_excecao, name='getDisponExcecaoId'),
+    path('postDisponExcecao', DisponExcecaoViewSet.post_disponibilidade_excecao, name='postDisponExcecao'),
+    path('patchDisponExcecao/<int:id>/', DisponExcecaoViewSet.patch_disponibilidade_excecao, name='patchDisponExcecao'),
+    path('deleteDisponExcecao/<int:id>/', DisponExcecaoViewSet.delete_disponibilidade_excecao, name='deleteDisponExcecao'),
+]
--- api/disponibilidadeEncomenda/views.py
+++ api/disponibilidadeEncomenda/views.py
@@ -1,3 +1,85 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import *
+from .models import *
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+class DisponibilidadeViewSet(ModelViewSet):
+    queryset = Disponibilidade.objects.all()
+    serializer_class = DisponibilidadeSerializer
+    
+    @api_view(['GET'])
+    def list_disponibilidade(request):
+        disponibilidade = Disponibilidade.objects.all()
+        serializer = DisponibilidadeSerializer(disponibilidade, many=True)
+        return Response(serializer.data)
+    
+    @api_view(['GET'])
+    def get_disponibilidade(request, pk):
+        disponibilidade = Disponibilidade.objects.get(pk=pk)
+        serializer = DisponibilidadeSerializer(disponibilidade)
+        return Response(serializer.data)
+    
+    @api_view(['POST'])
+    def post_disponibilidade(request):
+        serializer = DisponibilidadeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    @api_view(['PATCH'])
+    def patch_disponibilidade(request, pk):
+        disponibilidade = Disponibilidade.objects.get(pk=pk)
+        serializer = DisponibilidadeSerializer(disponibilidade, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    @api_view(['DELETE'])
+    def delete_disponibilidade(request, pk):
+        disponibilidade = Disponibilidade.objects.get(pk=pk)
+        disponibilidade.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class DisponExcecaoViewSet(ModelViewSet):
+    queryset = DisponExcecao.objects.all()
+    serializer_class = DisponExcecaoSerializer
+
+    @api_view(['GET'])
+    def list_disponibilidade_excecao(request):
+        disponibilidade_excecao = DisponExcecao.objects.all()
+        serializer = DisponExcecaoSerializer(disponibilidade_excecao, many=True)
+        return Response(serializer.data)
+    
+    @api_view(['GET'])
+    def get_disponibilidade_excecao(request, pk):
+        disponibilidade_excecao = DisponExcecao.objects.get(pk=pk)
+        serializer = DisponExcecaoSerializer(disponibilidade_excecao)
+        return Response(serializer.data)
+    
+    @api_view(['POST'])
+    def post_disponibilidade_excecao(request):
+        serializer = DisponExcecaoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    @api_view(['PATCH'])
+    def patch_disponibilidade_excecao(request, pk):
+        disponibilidade_excecao = DisponExcecao.objects.get(pk=pk)
+        serializer = DisponExcecaoSerializer(disponibilidade_excecao, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    @api_view(['DELETE'])
+    def delete_disponibilidade_excecao(request, pk):
+        disponibilidade_excecao = DisponExcecao.objects.get(pk=pk)
+        disponibilidade_excecao.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
