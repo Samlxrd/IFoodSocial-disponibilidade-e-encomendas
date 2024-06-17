bd9398941d541a99c6348f7c15aa4de6fe0e998d
Samlxrd <samlord.dzn@gmail.com>
2024-06-17 18:49:29 -0300

    header component

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -5,19 +5,54 @@ import { RouterLink } from 'vue-router';
 
 <template>
   <header>
+    <h2><a href="/">IFoodSocial</a></h2>
     <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/other">Outro</RouterLink>
-        <a href="">Disponibilidade</a>
-        <a href="">Pedidos</a>
-        <a href="">Atendimento</a>
-        <a href="">Entregas</a>
-        <a href="">Agendamento</a>
-        <a href="">Login</a>
+        <RouterLink to="/" class="nav-item">Home</RouterLink>
+        <RouterLink to="/other" class="nav-item">Outro</RouterLink>
+        <RouterLink to="/disponibilidade" class="nav-item">Disponibilidade</RouterLink>
     </nav>
   </header>
 </template>
 
 <style scoped>
+  header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    background-color: var(--orange);
+    height: 8vh;
+    padding: 0vh 4vh;
+  }
 
+  h2 a {
+    color: var(--white);
+    text-decoration: none;
+  }
+
+  h2 {
+    transform: none;
+    transition: transform 0.2s ease-out;
+  }
+
+  h2:hover {
+    transform: translateX(2vh);
+  }
+
+  nav {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .nav-item {
+    margin: 0vh 2vh;
+    text-decoration: none;
+    color: var(--white);
+  }
+
+  .nav-item:hover {
+    text-decoration: underline;
+  }
+  
 </style>
