69a200c7a36475502b69396a2010abd714c7dc56
Samlxrd <samlord.dzn@gmail.com>
2024-06-17 18:50:00 -0300

    api functions

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,48 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/',
+    baseURL: 'http://localhost:8000/api/',
     headers: {
         'Content-Type': 'application/json'
     }
 });
 
-export default api;
+export async function getDisponibilidade() {
+    try {
+        const response = await api.get('/getDisponibilidade');
+        console.log('Dados ', response)
+        return response.data;
+    } catch (err) {
+        console.log('Falha ao buscar disponibilidade: ', err);
+    }
+}
+
+export async function getLocalidade() {
+    try {
+        const response = await api.get('/getLocalidade');
+        console.log('Dados ', response)
+        return response.data;
+    } catch (err) {
+        console.log('Falha ao buscar localidade: ', err);
+    }
+}
+
+export async function getEmpreendimento() {
+    try {
+        const response = await api.get('/getEmpreendimento');
+        console.log('Dados ', response)
+        return response.data;
+    } catch (err) {
+        console.log('Falha ao buscar empreendimento: ', err);
+    }
+}
+
+export async function postDisponibilidade(dados) {
+    try {
+        console.log('Dados enviados:', dados);
+        const response = await api.post('/postDisponibilidade', dados);
+        return response;
+    } catch (err) {
+        console.log('Falha ao enviar disponibilidade: ', err);
+    }
+}
